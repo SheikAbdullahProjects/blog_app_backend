34326c7486a104d417c90a5f951fc539a15c4f38
Sheik Abdullah <sheikabdullah.web@gmail.com>
2025-09-04 13:40:30 +0530

    ree commit

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://blog-app-frontend-08qd.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
